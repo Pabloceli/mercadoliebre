753f8ba5b89acd38eb35703a02be478fb33f9a73
Pabloceli <pabloignacio.celi@gmail.com>
2021-05-06 23:41:03 -0300

    agregue config para heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(3030, () => {console.log("Servidor corriendo http://localhost:3030");});
+app.listen(process.env.PORT || 3000, () => {console.log("Servidor corriendo http://localhost:3000");});
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "/views/home.html"));});
 app.get("/home", (req, res) => {res.sendFile(path.join(__dirname, "/views/home.html"));});
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "/views/login.html"));});
